9d7d3a270a6f038a9c7a99d48dcb89fb5699c267
Kostas Stroggylos <afdempkstr@users.noreply.github.com>
2020-04-10 21:13:24 +0300

    TShirt

---
+++ StrategyAssignment/Variation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyAssignment
+{
+    abstract class Variation
+    {
+        public abstract decimal Cost(TShirt tshirt);
+    }
+
+    class ColorVariation : Variation
+    {
+        public override decimal Cost(TShirt tshirt)
+        {
+            switch (tshirt.Color)
+            {
+                case Color.BLUE:
+                    tshirt.Price += 0.5m;
+                    break;
+
+                case Color.GREEN:
+                    tshirt.Price += 0.75m;
+                    break;
+
+                default:
+                    tshirt.Price += 1.05m;
+                    break;
+            }
+
+            return tshirt.Price;
+        }
+    }
+
+}

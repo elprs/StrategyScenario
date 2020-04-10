0cb4b72933870e7246a0a0939977aeac61c52741
Kostas Stroggylos <afdempkstr@users.noreply.github.com>
2020-04-10 22:08:16 +0300

    Variations done, pending payment methods

--- StrategyAssignment/Program.cs
+++ StrategyAssignment/Program.cs
@@ -12,4 +12,24 @@ namespace StrategyAssignment
         {
         }
     }
+
+    class Eshop
+    {
+        //private PaymentMethod _paymentMethod;
+
+        private IEnumerable<Variation> _variations;
+
+        public void PayTShirt(TShirt tshirt)
+        {
+            foreach (var variation in _variations)
+            {
+                Console.WriteLine($"Applying {variation.GetType().Name}");
+                variation.Cost(tshirt);
+                Console.WriteLine($"TShirt cost after applying {variation.GetType().Name} is: {tshirt.Price}");
+            }
+
+            //_paymentMethod.Pay(tshirt.Price);
+        }
+
+    }
 }
--- StrategyAssignment/Variation.cs
+++ StrategyAssignment/Variation.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace StrategyAssignment
 {
@@ -10,6 +6,11 @@ namespace StrategyAssignment
     {
         public abstract decimal Cost(TShirt tshirt);
     }
+
+    //abstract class Variation<T>
+    //{
+    //    public abstract decimal Cost<T>(T item);
+    //}
 
     class ColorVariation : Variation
     {
@@ -34,4 +35,53 @@ namespace StrategyAssignment
         }
     }
 
+    class SizeVariation : Variation
+    {
+        private static Dictionary<Size, decimal> _sizeCosts;
+
+        static SizeVariation()
+        {
+            _sizeCosts = new Dictionary<Size, decimal>()
+            {
+                { Size.XS, -1m },
+                { Size.S, -0.5m },
+                { Size.M, 0m },
+                { Size.L, 0.5m },
+                { Size.XL, 1m },
+                { Size.XXL, 2m },
+                { Size.XXXL, 3.5m }
+            };
+        }
+
+        public override decimal Cost(TShirt tshirt)
+        {
+            tshirt.Price += _sizeCosts[tshirt.Size];
+            return tshirt.Price;
+        }
+    }
+
+    class FabricVariation : Variation
+    {
+        private static Dictionary<Fabric, decimal> _fabricVariations;
+
+        static FabricVariation()
+        {
+            _fabricVariations = new Dictionary<Fabric, decimal>()
+            {
+                { Fabric.CASHMERE, 15m },
+                { Fabric.COTTON, 5m },
+                { Fabric.LINEN, 9m },
+                { Fabric.POLYESTER, 6m },
+                { Fabric.RAYON, 8m },
+                { Fabric.SILK, 22m },
+                { Fabric.WOOL, 12m }
+            };
+        }
+
+        public override decimal Cost(TShirt tshirt)
+        {
+            tshirt.Price += _fabricVariations[tshirt.Fabric];
+            return tshirt.Price;
+        }
+    }
 }
